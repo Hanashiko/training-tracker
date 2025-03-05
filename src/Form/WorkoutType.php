f957cd1814bfba33d0c580d0a808f30ccce8b339
Hanashiko <hlichisper@gmail.com>
2025-03-05 14:30:45 +0200

    add forms

---
+++ src/Form/WorkoutType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Workout;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WorkoutType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('notes')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Workout::class,
+        ]);
+    }
+}
