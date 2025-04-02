1a6f608ce6eb4ac389fb6cd4bf99cfb39a40d367
Hanashiko <hlichisper@gmail.com>
2025-04-02 18:03:09 +0300

    add faker; create fixtues file for exercise

---
+++ src/DataFixtures/ExerciseFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Exercise;
+use App\Entity\MuscleGroup;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ExerciseFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        
+    }
+}
